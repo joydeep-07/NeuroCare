1a06bcf3d1200bb6c7ac34cb46db9c852b3018a0
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-07-29 08:27:20 +0530

    Footer Removed from AI Page

--- client/src/layouts/Root.tsx
+++ client/src/layouts/Root.tsx
@@ -1,15 +1,19 @@
-import Footer from './Footer'
-import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Root = () => {
+  const location = useLocation();
+
+  const hideFooter = location.pathname === "/ai-assistant";
+
   return (
     <div>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
+      <Navbar />
+      <Outlet />
+      {!hideFooter && <Footer />}
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
